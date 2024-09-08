123e68ea3cae8f8446bc0d9c2191c7bdceb4c8bd
VitorHugoFMartins <vitorferreira010vhfm@gmail.com>
2024-09-08 16:56:43 -0300

    Teste técnico

---
+++ Faturamento.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TesteTargetSistemas
+{
+    public class Faturamento
+    {
+        public int Dia { get; set; }
+        public decimal Valor { get; set; }
+
+		public void ResultadoFaturamento()
+		{
+			string jsonPath = "dados.json";
+
+			// Deserializar o JSON para uma lista de objetos Faturamento
+			string json = File.ReadAllText(jsonPath);
+		
+				List<Faturamento> faturamentos = JsonConvert.DeserializeObject<List<Faturamento>>(json);
+				// Calcular a média, ignorando dias sem faturamento
+				if(faturamentos == null)
+				{
+					Console.WriteLine("O arquivo JSON não é válido");
+					return;
+				}
+
+				decimal soma = faturamentos.Where(x => x.Valor > 0).Sum(x => x.Valor);
+				int diasComFaturamento = faturamentos.Count(x => x.Valor > 0);
+				decimal media = diasComFaturamento > 0 ? soma / diasComFaturamento : 0;
+
+				// Encontrar o menor e o maior valor
+				decimal menorValor = faturamentos.Where(x => x.Valor > 0).Min(x => x.Valor);
+				decimal maiorValor = faturamentos.Where(x => x.Valor > 0).Max(x => x.Valor);
+
+				var menorData = faturamentos.First(x => x.Valor == menorValor);
+				var maiorData = faturamentos.First(x => x.Valor == maiorValor);
+				
+
+			// Contar os dias com faturamento acima da média
+				int diasAcimaDaMedia = faturamentos.Count(x => x.Valor > media);
+				Console.WriteLine($"Menor valor: R$ {menorValor} no dia {menorData.Dia}");
+				Console.WriteLine($"Maior valor: R$ {maiorValor} no dia {maiorData.Dia}");
+				Console.WriteLine($"Número de dias com faturamento acima da média: {diasAcimaDaMedia}");
+			
+		}
+	}
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using TesteTargetSistemas;
+#region Exercicio 1
+//int indice = 13;
+//int soma = 0;
+//int k = 0;
+
+//while(k < indice)
+//{
+//	k = k + 1;
+//	soma = soma + k;
+//}
+//Console.WriteLine(soma);
+#endregion
+#region Exercicio 2
+//Console.WriteLine("Insira um valor");
+//var inputValue = Console.ReadLine();
+
+//if (inputValue != null)
+//{
+//	if (inputValue.All(char.IsDigit))
+//	{
+//		int value = Convert.ToInt32(inputValue);
+//		int a = 0;
+//		int b = 1;
+
+//		while (b < value)
+//		{
+//			int temp = a;
+//			a = b;
+//			b = temp + b;
+//		}
+
+//		if (b == value)
+//		{
+//			Console.WriteLine($"O número {value} pertence a sequência de Fibonacci");
+//		}
+//		else
+//		{
+//			Console.WriteLine($"O número {value} não pertence a sequência de Fibonacci");
+//		}
+//	}
+//	else
+//	{
+//		Console.WriteLine("Insira apenas números");
+//		return;
+//	}
+//}
+//else
+//{
+//	Console.WriteLine("O número inserido näo é válido");
+//	return;
+//}
+#endregion
+#region Exercicio 3
+//Faturamento faturamento = new Faturamento();
+
+//faturamento.ResultadoFaturamento();
+#endregion
+#region Exercicio 4	
+
+//double[] faturamento = { 67836.43, 36678.66, 29229.88, 27165.48, 19849.53 };
+//double valorTotal = faturamento.Sum();
+
+//Console.WriteLine($"Faturamento Mensal de SP: R${faturamento[0]}");
+//Console.WriteLine($"Faturamento Mensal de RJ: R${faturamento[1]}");
+//Console.WriteLine($"Faturamento Mensal de MG: R${faturamento[2]}");
+//Console.WriteLine($"Faturamento Mensal de ES: R${faturamento[3]}");
+//Console.WriteLine($"Faturamento Mensal de outros estados: R${faturamento[4]}");
+//Console.WriteLine($"Faturamento total: R${valorTotal}");
+//Console.WriteLine("\n");
+
+//List<double> percentual = [];
+
+//for(int i = 0; i< faturamento.Length; i++)
+//{
+//	percentual.Add(faturamento[i] / valorTotal * 100);
+//}
+//Console.WriteLine($"Percentual de SP: {percentual[0]:F2}%");
+//Console.WriteLine($"Percentual de RJ: {percentual[1]:F2}%");
+//Console.WriteLine($"Percentual de MG: {percentual[2]:F2}%");
+//Console.WriteLine($"Percentual de ES: {percentual[3]:F2}%");
+//Console.WriteLine($"Percentual de outros estados: {percentual[4]:F2}%");
+
+#endregion
+#region Exercicio 5
+//Console.WriteLine("Insira um texto: ");
+//var texto = Console.ReadLine();
+//string textoInvertido = "";
+
+//if (texto == null) {
+//	Console.WriteLine("Insira um texto válido");
+//	return;
+//}
+//else
+//{
+//	for (int i = texto.Length - 1; i >= 0; i--)
+//	{
+//		textoInvertido += texto[i];
+//	}
+
+//	Console.WriteLine(textoInvertido);
+//}
+#endregion
